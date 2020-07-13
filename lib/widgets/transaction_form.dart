c4d44df1f02d27e2b4e7fc885f422ee9587fa335
Nicolas Fortin <nicolas.fortin37@gmail.com>
2020-07-13 18:57:18 +0200

    Responsible add modal

--- lib/widgets/transaction_form.dart
+++ lib/widgets/transaction_form.dart
@@ -20,55 +20,62 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Title',
-            ),
-            controller: _titleController,
-            onSubmitted: (_) => _submitData(),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Amount',
-            ),
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            onSubmitted: (_) => _submitData(),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: _selectedDate == null
-                    ? Text('No Date Chosen')
-                    : Text(
-                        'Picked Date: ${DateFormat.yMMMEd().format(_selectedDate)}'),
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 10,
+          left: 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          right: 10,
+        ),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
               ),
-              FlatButton(
-                textColor: Theme.of(context).primaryColor,
-                onPressed: _showDatePicker,
-                child: Text(
-                  'Choose Date',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Amount',
+              ),
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _submitData(),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: _selectedDate == null
+                      ? Text('No Date Chosen')
+                      : Text(
+                          'Picked Date: ${DateFormat.yMMMEd().format(_selectedDate)}'),
+                ),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  onPressed: _showDatePicker,
+                  child: Text(
+                    'Choose Date',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20),
-            child: RaisedButton(
-              child: Text('Add Transaction'),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-              onPressed: _submitData,
+              ],
             ),
-          ),
-        ],
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: RaisedButton(
+                child: Text('Add Transaction'),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+                onPressed: _submitData,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
