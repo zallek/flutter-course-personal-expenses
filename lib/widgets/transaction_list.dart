fafdde33ff37135d71b833067cdcaa8ac6ba2e0d
Nicolas Fortin <nicolas.fortin37@gmail.com>
2020-07-13 13:25:25 +0200

    No transactions screen

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,15 +15,34 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return TransactionCard(
-            key: Key(transactions[index].id),
-            transaction: transactions[index],
-          );
-        },
-        itemCount: transactions.length,
-      ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 40),
+                ),
+                Container(
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                  height: 160,
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return TransactionCard(
+                  key: Key(transactions[index].id),
+                  transaction: transactions[index],
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
